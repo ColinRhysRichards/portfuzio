48632f65174d2de1f48fc22c94d2fef02635343f
colin richards <colin.rhys.richards@gmail.com>
2019-07-12 13:45:05 -0600

    test # 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <h1>Testing Testing</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
